3a8d2e599a85f8244af51e362ca3bc1102a09606
Aakriti Budhraja <aakriti.budhraja@gmail.com>
2020-10-26 15:22:59 +0530

    Update maximum_profit.cpp

--- maximum_profit.cpp
+++ maximum_profit.cpp
@@ -1,6 +1,8 @@
 // Given an array of prices of a particular stock, calculate the maximum profit that you can earn by buying and selling of stocks.
 // Multiple stocks can only be bought serially and not parallely, i.e. you can only buy after you have sold the previously bought stock.
 
+// O(n) time and O(1) space
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
