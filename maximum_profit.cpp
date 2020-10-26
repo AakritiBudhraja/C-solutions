623612dd8b083078b1d0831a36f8c8c107d3afe2
Aakriti Budhraja <aakriti.budhraja@gmail.com>
2020-10-26 15:20:14 +0530

    Add files via upload

---
+++ maximum_profit.cpp
@@ -0,0 +1,34 @@
+// Given an array of prices of a particular stock, calculate the maximum profit that you can earn by buying and selling of stocks.
+// Multiple stocks can only be bought serially and not parallely, i.e. you can only buy after you have sold the previously bought stock.
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max_profit = 0;
+        for(auto it=prices.begin()+1; it!=prices.end(); it++)
+        {
+            auto prev_price = *(it-1);
+            auto curr_price = *it;
+            if(curr_price>prev_price)
+            {
+                auto curr_profit = curr_price-prev_price;
+                max_profit += curr_profit;
+            }
+        }
+        
+        return max_profit;
+    }
+};
+
+int main()
+{
+    vector<int> v = {7,2,5,2,3,7};
+    Solution s;
+    int ans = s.maxProfit(v);
+    cout<<"Maximum profit is: "<<ans<<endl;
+}
